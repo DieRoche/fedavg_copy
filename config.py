1ac94fea80bf574bbe7727ff1cbe6b10c86029e0
DieRoche <67673036+DieRoche@users.noreply.github.com>
2026-05-19 22:06:26 -0600

    Add nnz upload traffic metric and configurable wandb project

--- config.py
+++ config.py
@@ -74,6 +74,7 @@ def get_config():
         ),
     )
     parser.add_argument("--wandb_enabled", type=str2bool, default=True)
+    parser.add_argument("--wandb_project", type=str, default="Gauss-Southwell")
 
     parser.add_argument("--device", type=str, default="cuda")
 
